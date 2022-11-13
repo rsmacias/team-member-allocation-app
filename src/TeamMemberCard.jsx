d7a61f90c7df7bbef2df0a3fb0298c26bcfba25c
Robert Macias <rsmacias@gmail.com>
2022-11-13 14:02:53 -0500

    Abstract Responsibilities Into Functional Components: > TeamMemberCard component for managing each card information.

---
+++ src/TeamMemberCard.jsx
@@ -0,0 +1,22 @@
+import femaleProfile from './images/femaleProfile.jpg';
+import maleProfile from './images/maleProfile.jpg';
+
+const TeamMemberCard = ({selectedTeam, handleEmployeeCardClick, employee}) => {
+    return (
+        <div key={employee.id}
+                id={employee.id} 
+                className={(employee.teamName === selectedTeam? 'card m-2 standout' : 'card m-2')} 
+                style={{ cursor: "pointer"}} 
+                onClick={handleEmployeeCardClick}>
+            <img src={ employee.gender === 'female'? femaleProfile : maleProfile } 
+                 className='card-img-top'
+                 alt={employee.fullName} />
+            <div className='card-body'>
+                <h5 className="card-tittle">Full Name: {employee.fullName}</h5>
+                <p className="card-text"><b>Designation:</b> {employee.designation}</p>
+            </div>
+        </div>
+    )
+}
+
+export default TeamMemberCard;
--- src/TeamMembers.jsx
+++ src/TeamMembers.jsx
@@ -1,20 +1,12 @@
-import femaleProfile from './images/femaleProfile.jpg';
-import maleProfile from './images/maleProfile.jpg';
+import TeamMemberCard from './TeamMemberCard';
 
 const TeamMembers = ({employees, selectedTeam, handleEmployeeCardClick}) => {
     return (
         employees.map((employee) => (
-            <div key={employee.id}
-                    id={employee.id} 
-                    className={(employee.teamName === selectedTeam? 'card m-2 standout' : 'card m-2')} 
-                    style={{ cursor: "pointer"}} 
-                    onClick={handleEmployeeCardClick}>
-                <img src={ employee.gender == 'female'? femaleProfile : maleProfile } className='card-img-top' />
-                <div className='card-body'>
-                    <h5 className="card-tittle">Full Name: {employee.fullName}</h5>
-                    <p className="card-text"><b>Designation:</b> {employee.designation}</p>
-                </div>
-            </div>
+            <TeamMemberCard 
+                handleEmployeeCardClick={handleEmployeeCardClick}
+                selectedTeam={selectedTeam}
+                employee={employee} />
         ))
     );
 }
