60a82d6ed806a46fdda1bc153d2670bef0256b7e
Robert Macias <rsmacias@gmail.com>
2022-11-13 01:31:20 -0500

    Adding useEffect hook in order to store and update the state inside the localStorage. Besides this, updating the initialization of the states with the values from the localStorage. Using 'short-circuit validation' for this purpose.

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,22 @@ import Header from './Header';
 import Employees from './Employees';
 import Footer from './Footer';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import data from './data.json';
 
 function App() {
 
-  const [employees, setEmployees] = useState([...data]);
-  const [selectedTeam, setTeam] = useState("TeamB");
+  const [employees, setEmployees] = useState(JSON.parse(localStorage.getItem('employeeList')) || [...data]);
+  const [selectedTeam, setTeam] = useState(JSON.parse(localStorage.getItem('selectedItem')) || "TeamB");
+
+  useEffect(() => {
+    // How to read this method: When employees state changes, we update the local storage 
+    localStorage.setItem('employeeList', JSON.stringify(employees));
+  }, [employees]);
+
+  useEffect(() => {
+    localStorage.setItem('selectedItem', JSON.stringify(selectedTeam));
+  }, [selectedTeam]);
 
   function handleTeamSelectionChange (event) {
       console.log(event.target.value);
