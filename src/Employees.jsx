4f81062467460387266c64f3c5d3d7c2c3de8023
Robert Macias <rsmacias@gmail.com>
2022-11-12 23:25:14 -0500

    Adding functionality for adding a member to a team and removing a member from a team just by clicking the image of the member. Using css box shadow to show this functionality.

--- src/Employees.jsx
+++ src/Employees.jsx
@@ -6,12 +6,35 @@ import maleProfile from './images/maleProfile.jpg';
 const Employees = () => {
 
     const [employees, setEmployees] = useState([...data]);
+    const [selectedTeam, setTeam] = useState("TeamB");
+
+    function handleTeamSelectionChange (event) {
+        console.log(event.target.value);
+        setTeam(event.target.value);
+    }
+
+    function handleEmployeeCardClick (event) {
+        console.log(event.target.value);
+        const transformedEmployees = employees.map((employee) => {
+            if(employee.id === parseInt(event.currentTarget.id)) {
+                if(employee.teamName === selectedTeam) {
+                    return { ...employee, teamName: ''}
+                } else {
+                    return { ...employee, teamName: selectedTeam };
+                }
+            } else {
+                return employee;
+            } 
+        });
+
+        setEmployees(transformedEmployees);
+    }
 
     return (
         <main className='container'>
             <div className="row justify-content-center mt-3 mb-3">
                 <div className="col-6">
-                    <select className='form-select form-select-lg'>
+                    <select className='form-select form-select-lg' value={selectedTeam} onChange={handleTeamSelectionChange}>
                         <option value="TeamA">TeamA</option>
                         <option value="TeamB">TeamB</option>
                         <option value="TeamC">TeamC</option>
@@ -25,7 +48,10 @@ const Employees = () => {
                     <div className="card-collection">
                     {
                         employees.map((employee) => (
-                            <div id={employee.id} className='card m-2' style={{ cursor: "pointer"}}>
+                            <div id={employee.id} 
+                                 className={(employee.teamName === selectedTeam? 'card m-2 standout' : 'card m-2')} 
+                                 style={{ cursor: "pointer"}} 
+                                 onClick={handleEmployeeCardClick}>
                                 <img src={ employee.gender == 'female'? femaleProfile : maleProfile } className='card-img-top' />
                                 <div className='card-body'>
                                     <h5 className="card-tittle">Full Name: {employee.fullName}</h5>
