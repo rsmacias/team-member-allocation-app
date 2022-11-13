3d280d16372beb5bef5ea896463ac392dd00205a
Robert Macias <rsmacias@gmail.com>
2022-11-13 13:22:22 -0500

    Refinement: > Removing duplicated logic while resolving the groupTeamMembers array. > Change 'members' to 'member' when there is just one member in a team.

--- src/GroupedTeamMembers.jsx
+++ src/GroupedTeamMembers.jsx
@@ -5,23 +5,16 @@ const GroupedTeamMembers = ({employees, selectedTeam, setTeam}) => {
     const [groupedEmployees, setGroupedData] = useState(groupTeamMembers());
 
     function groupTeamMembers() {
+        const allowedTeams = ['TeamA', 'TeamB', 'TeamC', 'TeamD']; // This could be fetched by an API call
         let teams = [];
 
-        const teamAMembers = employees.filter((employee) => employee.teamName === 'TeamA');
-        const teamA = {team:'TeamA', members:teamAMembers, collapsed: selectedTeam === 'TeamA'?false:true};
-        teams.push(teamA);
-
-        const teamBMembers = employees.filter((employee) => employee.teamName === 'TeamB');
-        const teamB = {team:'TeamB', members:teamBMembers, collapsed: selectedTeam === 'TeamB'?false:true};
-        teams.push(teamB);
-
-        const teamCMembers = employees.filter((employee) => employee.teamName === 'TeamC');
-        const teamC = {team:'TeamC', members:teamCMembers, collapsed: selectedTeam === 'TeamC'?false:true};
-        teams.push(teamC);
-
-        const teamDMembers = employees.filter((employee) => employee.teamName === 'TeamD');
-        const teamD = {team:'TeamD', members:teamDMembers, collapsed: selectedTeam === 'TeamD'?false:true};
-        teams.push(teamD);
+        teams = allowedTeams.map(team => {
+            return {
+                team:team, 
+                members:employees.filter((employee) => employee.teamName === team), 
+                collapsed: selectedTeam === team ? false : true
+            };
+        });
 
         return teams;
     }
