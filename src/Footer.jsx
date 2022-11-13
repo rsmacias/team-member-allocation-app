0331ad39c323802ed8d55139bc1f5e08c6524dd9
Robert Macias <rsmacias@gmail.com>
2022-11-13 00:06:37 -0500

    Beautying the Footer component.

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,14 @@
 const Footer = () => {
+
+    const today = new Date();
+
     return (
-        <footer>
-            <h1>Footer</h1>
+        <footer className="container">
+            <div className="row justify-content-center mt-3 mb-4">
+                <div className="col-8"></div>
+                    <h5>Team Member Allocation App - {today.getFullYear()}</h5>
+                </div>
+            <div/>
         </footer>
     );
 }
